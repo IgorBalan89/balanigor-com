034910b65f7620274394e034d329b31842b37757
balanigor89 <balanigor89@gmail.com>
2025-06-23 15:11:51 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/igor-portfolio",
+  base: "/balanigor-com",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
