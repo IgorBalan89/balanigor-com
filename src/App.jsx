049bf7af5855a1c0fe75a4aea04f8393ab217571
balanigor89 <balanigor89@gmail.com>
2025-06-23 16:24:58 +0100

    removed route

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
