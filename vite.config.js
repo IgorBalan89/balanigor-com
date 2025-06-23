e30aa9e57a7b8e1d36f49d0ac6051e257611d02b
balanigor89 <balanigor89@gmail.com>
2025-06-23 16:10:43 +0100

    changes in config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/balanigor-com",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
