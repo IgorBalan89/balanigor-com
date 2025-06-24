1f0af0328b35603eb5989f87369fa99e31694b2b
balanigor89 <balanigor89@gmail.com>
2025-06-24 16:37:04 +0100

    main.jsx file changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./pages/Home.jsx";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NotFound } from "./pages/NotFound.jsx";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>
+  <BrowserRouter basename="/balanigor-com">
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
