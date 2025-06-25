b58896b6ba4501d464c17aee5da9598fc114bb27
balanigor89 <balanigor89@gmail.com>
2025-06-25 10:01:35 +0100

    file change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Home } from "./pages/Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./pages/NotFound.jsx";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/balanigor-com">
+  <BrowserRouter>
     <Routes>
       <Route index element={<Home />} />
       <Route path="*" element={<NotFound />} />
