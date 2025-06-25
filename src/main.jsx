0d0b4950483e25abb29231dca17cf9c2daa119e3
balanigor89 <balanigor89@gmail.com>
2025-06-25 10:33:37 +0100

    main.jsx changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./pages/Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/balanigor-com">
     <Routes>
-      <Route index element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
