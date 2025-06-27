fbd2fa0e0cc97fc3b32ecd809d4fa19a49ab1c0f
balanigor89 <balanigor89@gmail.com>
2025-06-27 10:42:56 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/balanigor-com/",
+  base: process.env.VITE_BASE_PATH || "/balanigor-com",
+
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  }
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
 });
